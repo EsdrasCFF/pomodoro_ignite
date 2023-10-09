d7459146e73cac082f259955afb42c27fe400021
EsdrasCFF <ecff.dev@gmail.com>
2023-10-09 13:43:34 -0300

    Style: Create Theme Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+import { ThemeProvider } from 'styled-components';
 import { Button } from "./components/Button";
+
+import { GlobalStyle } from './styles/global';
+import  theme from './styles/themes/default';
 
 export function App() {
 
   return (
 
-    <>
+    <ThemeProvider theme={theme} >
       <Button variant="secundary" />
       <Button variant="primary" />
       <Button variant="danger" />
       <Button variant="success"/>
-    </>
+
+      <GlobalStyle />
+    </ThemeProvider>
 
     )
 }
