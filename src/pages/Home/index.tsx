5f20f9202b018704041a5de4c4aa0fd0b675cf05
EsdrasCFF <ecff.dev@gmail.com>
2023-10-10 23:33:34 -0300

    Feat: Inversion Dependency

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,7 +5,9 @@ import { useEffect, useState } from "react";
 import { differenceInSeconds } from "date-fns"
 import * as zod from "zod"
 
-import { HomeContainer , FormContainer, CountdownContainer, Separator, StartCountdownButton, TaskInput, MinutesAmountInput, StopCountdownButton} from "./styles";
+import { HomeContainer, StartCountdownButton, StopCountdownButton} from "./styles";
+import { NewCycleForm } from "./components/NewCycleForm";
+import { Countdown } from "./components/Countdown";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a Tarega que deseja realizar!"),
@@ -22,7 +24,8 @@ interface ICycle {
   task: string,
   minutesAmount: number,
   startDate: Date,
-  interruptedDate?: Date
+  interruptedDate?: Date,
+  finishedDate?: Date 
 }
 
 export function Home() {
@@ -69,7 +72,7 @@ export function Home() {
   }
 
   function handleInterruptedCycle() {
-    setCycles(cycles.map(cycle => {
+    setCycles((state) => state.map(cycle => {
       if(cycle.id === activeCycleId) {
         return { ...cycle, interruptedDate: new Date() }
       } else {
@@ -86,15 +89,31 @@ export function Home() {
 
     if (activeCycle) {
       interval = setInterval(() => {
-        setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
+        const differenceSeconds = differenceInSeconds(new Date(), activeCycle.startDate);
+
+        if(differenceSeconds >= totalSeconds) {
+          setCycles((state) => 
+            state.map((cycle) => {
+              if (cycle.id === activeCycleId) {
+                return { ...cycle, finishedDate: new Date() }
+              } else {
+                return cycle
+              }
+            }),
+          )
+          
+          setAmountSecondsPassed(totalSeconds)
+          clearInterval(interval)
+        } else {
+          setAmountSecondsPassed(differenceSeconds)
+        }
       }, 1000)
     }
 
     return () => {
       clearInterval(interval)
-      setAmountSecondsPassed(0)
     }
-  }, [activeCycle])
+  }, [activeCycle, activeCycleId, totalSeconds])
 
   useEffect(() => {
     if(activeCycle) {
@@ -105,47 +124,10 @@ export function Home() {
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)} >
-        <FormContainer>
-          <label htmlFor="task" >Vou trabalhar em</label>
-          <TaskInput
-            id="task" 
-            list="task-suggestions" 
-            placeholder="Dê um nome para o seu projeto"
-            disabled={!!activeCycle}
-            {...register("task")}
-          />
+        
 
-          <datalist  id="task-suggestions">
-            <option value="Projeto 1" />
-            <option value="Projeto 2" />
-            <option value="Projeto 3" />
-            <option value="Projeto 4" />
-
-          </datalist>
-
-          <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput 
-            type="number" 
-            id="minutesAmount" 
-            placeholder="00" 
-            step={5}
-            min={5}
-            max={60}
-            disabled={!!activeCycle}
-            {...register("minutesAmount", {valueAsNumber: true})}
-          />
-          
-          <span>minutos.</span>
-        </FormContainer>
-
-        <CountdownContainer>
-          <span>{minutes[0]}</span>
-          <span>{minutes[1]}</span>
-          <Separator>:</Separator>
-          <span>{seconds[0]}</span>
-          <span>{seconds[1]}</span>
-
-        </CountdownContainer>
+        <NewCycleForm/>
+        <Countdown/>
 
         { activeCycle ? (
           <StopCountdownButton onClick={handleInterruptedCycle} type="button">
